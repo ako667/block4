c9ab46583b1c576328ade695ab866acca5eb3719
AktotyOmar240956 <240956@astanait.edu.kz>
2026-05-16 00:03:21 +0500

    Delete IChainlinkAdapter.sol

--- IChainlinkAdapter.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-interface IChainlinkAdapter {
-    function latestValidatedPrice() external view returns (int256 price, uint256 updatedAt);
-    function isStale() external view returns (bool);
-    function maxStaleness() external view returns (uint256);
-}
